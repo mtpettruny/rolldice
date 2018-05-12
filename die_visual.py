34e2695335ab53b03d23cc6137e9b3d87387a5c1
mrpoopybutthole <mtpettruny@gmail.com>
2018-05-12 14:56:46 -0400

    update for histogram and 5 dice

--- die_visual.py
+++ die_visual.py
@@ -1,21 +1,37 @@
 from die import Die
 import time
+import pygal
 
 start = time.time()
 # Create a D6
-die = Die()
+die1 = Die()
+die2 = Die()
+die3 = Die()
+die4 = Die()
+die5 = Die()
 
 results = []
-for roll_num in range(10000000):
-	result = die.roll()
+for roll_num in range(1000000000):
+	result = die1.roll()+die2.roll()+die3.roll()+die4.roll()+die5.roll()
 	results.append(result)
 
 frequencies = []
-for value in range(1, die.num_sides+1):
+for value in range(1, 30):
 	frequency = results.count(value)
 	frequencies.append(frequency)
 
-print(frequencies)
+# print(frequencies)
+
+hist=pygal.Bar()
+
+hist.title = "Results of rolling one D26 10000000 times"
+hist.x_labels = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D26', frequencies)
+hist.render_to_file('die_visual.svg')
+
 end = time.time()
 runtime = end-start
 print(runtime)
